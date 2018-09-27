d2d95f050c566f34f7280c7b9b93f048338c8c4f
nadinesk <nadine.khattak@gmail.com>
2018-09-27 10:10:21 -0700

    links

---
+++ src/components/Links.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import fetch from 'isomorphic-fetch'
+
+
+class Links extends Component {
+
+  	componentDidMount() {
+
+  	}
+
+	render() {
+		return (
+			<div className="App">
+        		<header className="App-header">         
+		          <h1 className="App-title">Welcome to Links</h1>
+		        </header>
+		        <p className="App-intro">
+		          To get started, edit <code>src/App.js</code> and save to reload.
+		        </p>
+		    </div>
+    	);
+  	}
+}
+
+export default Links;
